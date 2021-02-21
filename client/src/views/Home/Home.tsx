3711a8e354b10f4e7aef293ccf136412324ffada
dreamlancer <dreamlancer69@gmail.com>
2021-02-21 19:07:39 +1300

    :bug: fix build error

--- client/src/views/Home/Home.tsx
+++ client/src/views/Home/Home.tsx
@@ -1,19 +1,18 @@
-import React from "react";
-import styled from "styled-components";
-import { Heading, Text, BaseLayout } from "@pancakeswap-libs/uikit";
-import useI18n from "hooks/useI18n";
-import Page from "components/layout/Page";
-import FarmStakingCard from "views/Home/components/FarmStakingCard";
-import LotteryCard from "views/Home/components/LotteryCard";
-import CakeStats from "views/Home/components/CakeStats";
-import TotalValueLockedCard from "views/Home/components/TotalValueLockedCard";
-import EarnAPYCard from "views/Home/components/EarnAPYCard";
-import EarnAssetCard from "views/Home/components/EarnAssetCard";
-import WinCard from "views/Home/components/WinCard";
+import React from 'react'
+import styled from 'styled-components'
+import { Heading, Text, BaseLayout } from '@pancakeswap-libs/uikit'
+import useI18n from 'hooks/useI18n'
+import Page from 'components/layout/Page'
+import FarmStakingCard from 'views/Home/components/FarmStakingCard'
+import CakeStats from 'views/Home/components/CakeStats'
+import TotalValueLockedCard from 'views/Home/components/TotalValueLockedCard'
+import EarnAPYCard from 'views/Home/components/EarnAPYCard'
+import EarnAssetCard from 'views/Home/components/EarnAssetCard'
+import WinCard from 'views/Home/components/WinCard'
 
 const Hero = styled.div`
   align-items: center;
-  background-image: url("/images/pan-bg-mobile.svg");
+  background-image: url('/images/pan-bg-mobile.svg');
   background-repeat: no-repeat;
   background-position: top center;
   display: flex;
@@ -25,12 +24,12 @@ const Hero = styled.div`
   text-align: center;
 
   ${({ theme }) => theme.mediaQueries.lg} {
-    background-image: url("/images/pan-bg2.svg"), url("/images/pan-bg.svg");
+    background-image: url('/images/pan-bg2.svg'), url('/images/pan-bg.svg');
     background-position: left center, right center;
     height: 165px;
     padding-top: 0;
   }
-`;
+`
 
 const Cards = styled(BaseLayout)`
   align-items: stretch;
@@ -53,7 +52,7 @@ const Cards = styled(BaseLayout)`
       grid-column: span 6;
     }
   }
-`;
+`
 
 const CTACards = styled(BaseLayout)`
   align-items: start;
@@ -74,28 +73,22 @@ const CTACards = styled(BaseLayout)`
       grid-column: span 4;
     }
   }
-`;
+`
 
 const Home: React.FC = () => {
-  const TranslateString = useI18n();
+  const TranslateString = useI18n()
 
   return (
     <Page>
       <Hero>
         <Heading as="h1" size="xl" mb="24px" color="secondary">
-          {TranslateString(576, "PancakeSwap")}
+          {TranslateString(576, 'PancakeSwap')}
         </Heading>
-        <Text>
-          {TranslateString(
-            578,
-            "The #1 AMM and yield farm on Binance Smart Chain."
-          )}
-        </Text>
+        <Text>{TranslateString(578, 'The #1 AMM and yield farm on Binance Smart Chain.')}</Text>
       </Hero>
       <div>
         <Cards>
           <FarmStakingCard />
-          <LotteryCard />
         </Cards>
         <CTACards>
           <EarnAPYCard />
@@ -108,7 +101,7 @@ const Home: React.FC = () => {
         </Cards>
       </div>
     </Page>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
