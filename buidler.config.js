46532527cb8bccdb64fc23c2b02b4fc2aaa1319d
dreamlancer <dreamlancer69@gmail.com>
2021-02-22 09:17:52 +1300

    :fire: remove redundant comments

--- buidler.config.js
+++ buidler.config.js
@@ -1,8 +1,6 @@
 usePlugin("@nomiclabs/buidler-waffle");
 usePlugin("solidity-coverage");
 
-// This is a sample Buidler task. To learn how to create your own go to
-// https://buidler.dev/guides/create-task.html
 task("accounts", "Prints the list of accounts", async () => {
   const accounts = await ethers.getSigners();
 
@@ -13,38 +11,32 @@ task("accounts", "Prints the list of accounts", async () => {
 
 usePlugin("@nomiclabs/buidler-truffle5");
 
-// You have to export an object to set up your config
-// This object can have the following optional entries:
-// defaultNetwork, networks, solc, and paths.
-// Go to https://buidler.dev/config/ to learn more
 module.exports = {
-  // This is a sample solc configuration that specifies which version of solc to use
   solc: {
     version: "0.6.12",
     optimizer: {
       enabled: true,
-      runs: 200
-    }
+      runs: 200,
+    },
   },
 
   networks: {
-    buidlerevm: {
-    },
+    buidlerevm: {},
     development: {
       url: "http://127.0.0.1:7545",
       port: 7545,
-      network_id: "101"
+      network_id: "101",
     },
     test: {
       url: "http://127.0.0.1:7545",
       port: 7545,
-      network_id: "*"
+      network_id: "*",
     },
   },
   paths: {
     sources: "./contracts",
     tests: "./test",
     cache: "./cache",
-    artifacts: "./artifacts"
-  }
+    artifacts: "./artifacts",
+  },
 };
