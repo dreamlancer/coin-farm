55d645852d5226231c7a6e877b2fde7c706ed420
dreamlancer <dreamlancer69@gmail.com>
2021-02-21 21:00:12 +1300

    :rocket: add home page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,16 @@
 import React, { useEffect, Suspense, lazy } from 'react'
+import HomePage from 'pages/home/HomePage'
 import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom'
 import { useWallet } from '@binance-chain/bsc-use-wallet'
-import { ResetCSS } from '@pancakeswap-libs/uikit'
 import BigNumber from 'bignumber.js'
 import { useFetchProfile, useFetchPublicData } from 'state/hooks'
 import GlobalStyle from './style/Global'
-import Menu from './components/Menu'
 import ToastListener from './components/ToastListener'
 import PageLoader from './components/PageLoader'
 import Pools from './views/Pools'
 
 // Route-based code splitting
 // Only pool is included in the main bundle because of it's the most visited page'
-const Home = lazy(() => import('./views/Home'))
 const Farms = lazy(() => import('./views/Farms'))
 const NotFound = lazy(() => import('./views/NotFound'))
 
@@ -42,32 +40,29 @@ const App: React.FC = () => {
 
   return (
     <Router>
-      <ResetCSS />
       <GlobalStyle />
-      <Menu>
-        <Suspense fallback={<PageLoader />}>
-          <Switch>
-            <Route path="/" exact>
-              <Home />
-            </Route>
-            <Route path="/farms">
-              <Farms />
-            </Route>
-            <Route path="/pools">
-              <Pools />
-            </Route>
-            {/* Redirect */}
-            <Route path="/staking">
-              <Redirect to="/pools" />
-            </Route>
-            <Route path="/syrup">
-              <Redirect to="/pools" />
-            </Route>
-            {/* 404 */}
-            <Route component={NotFound} />
-          </Switch>
-        </Suspense>
-      </Menu>
+      <Suspense fallback={<PageLoader />}>
+        <Switch>
+          <Route path="/" exact>
+            <HomePage />
+          </Route>
+          <Route path="/farms">
+            <Farms />
+          </Route>
+          <Route path="/pools">
+            <Pools />
+          </Route>
+          {/* Redirect */}
+          <Route path="/staking">
+            <Redirect to="/pools" />
+          </Route>
+          <Route path="/syrup">
+            <Redirect to="/pools" />
+          </Route>
+          {/* 404 */}
+          <Route component={NotFound} />
+        </Switch>
+      </Suspense>
       <ToastListener />
     </Router>
   )
