6b07ed906e093a20a6015becf3eb6c9ee79f38da
dreamlancer <dreamlancer69@gmail.com>
2021-02-21 19:04:57 +1300

    :fire: remove nfts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,55 +1,49 @@
-import React, { useEffect, Suspense, lazy } from "react";
-import {
-  BrowserRouter as Router,
-  Redirect,
-  Route,
-  Switch,
-} from "react-router-dom";
-import { useWallet } from "@binance-chain/bsc-use-wallet";
-import { ResetCSS } from "@pancakeswap-libs/uikit";
-import BigNumber from "bignumber.js";
-import { useFetchProfile, useFetchPublicData } from "state/hooks";
-import GlobalStyle from "./style/Global";
-import Menu from "./components/Menu";
-import ToastListener from "./components/ToastListener";
-import PageLoader from "./components/PageLoader";
-import Pools from "./views/Pools";
+import React, { useEffect, Suspense, lazy } from 'react'
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom'
+import { useWallet } from '@binance-chain/bsc-use-wallet'
+import { ResetCSS } from '@pancakeswap-libs/uikit'
+import BigNumber from 'bignumber.js'
+import { useFetchProfile, useFetchPublicData } from 'state/hooks'
+import GlobalStyle from './style/Global'
+import Menu from './components/Menu'
+import ToastListener from './components/ToastListener'
+import PageLoader from './components/PageLoader'
+import Pools from './views/Pools'
 
 // Route-based code splitting
 // Only pool is included in the main bundle because of it's the most visited page'
-const Home = lazy(() => import("./views/Home"));
-const Farms = lazy(() => import("./views/Farms"));
-const Lottery = lazy(() => import("./views/Lottery"));
-const Ifos = lazy(() => import("./views/Ifos"));
-const NotFound = lazy(() => import("./views/NotFound"));
-const Nft = lazy(() => import("./views/Nft"));
-const Teams = lazy(() => import("./views/Teams"));
-const Team = lazy(() => import("./views/Teams/Team"));
-const Profile = lazy(() => import("./views/Profile"));
+const Home = lazy(() => import('./views/Home'))
+const Farms = lazy(() => import('./views/Farms'))
+const Lottery = lazy(() => import('./views/Lottery'))
+const Ifos = lazy(() => import('./views/Ifos'))
+const NotFound = lazy(() => import('./views/NotFound'))
+const Teams = lazy(() => import('./views/Teams'))
+const Team = lazy(() => import('./views/Teams/Team'))
+const Profile = lazy(() => import('./views/Profile'))
 
 // This config is required for number formating
 BigNumber.config({
   EXPONENTIAL_AT: 1000,
   DECIMAL_PLACES: 80,
-});
+})
 
 const App: React.FC = () => {
-  const { account, connect } = useWallet();
+  const { account, connect } = useWallet()
 
   // Monkey patch warn() because of web3 flood
   // To be removed when web3 1.3.5 is released
   useEffect(() => {
-    console.warn = () => null;
-  }, []);
+    console.warn = () => null
+  }, [])
 
   useEffect(() => {
-    if (!account && window.localStorage.getItem("accountStatus")) {
-      connect("injected");
+    if (!account && window.localStorage.getItem('accountStatus')) {
+      connect('injected')
     }
-  }, [account, connect]);
+  }, [account, connect])
 
-  useFetchPublicData();
-  useFetchProfile();
+  useFetchPublicData()
+  useFetchProfile()
 
   return (
     <Router>
@@ -72,9 +66,6 @@ const App: React.FC = () => {
             </Route>
             <Route path="/ifo">
               <Ifos />
-            </Route>
-            <Route path="/nft">
-              <Nft />
             </Route>
             <Route exact path="/teams">
               <Teams />
@@ -99,7 +90,7 @@ const App: React.FC = () => {
       </Menu>
       <ToastListener />
     </Router>
-  );
-};
+  )
+}
 
-export default React.memo(App);
+export default React.memo(App)
