38cd273502f2e888a02b627dcd617094409ef3f4
dreamlancer <dreamlancer69@gmail.com>
2021-02-21 22:41:40 +1300

    :rocket: add home header

--- client/src/pages/home/HomePage.tsx
+++ client/src/pages/home/HomePage.tsx
@@ -1,6 +1,7 @@
 import Header from 'components/Header'
 import React from 'react'
 import styled from 'styled-components'
+import watermelon from '../../assets/watermelon.png'
 
 const StyledHomePage = styled.div`
   position: fixed;
@@ -12,12 +13,57 @@ const StyledHomePage = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+  align-items: center;
+`
+
+const HeaderContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+type HeaderImageProps = {
+  left?: boolean
+  right?: boolean
+}
+
+const HeaderImage = styled.img`
+  height: 10rem;
+  transition: all 0.3s;
+  transform: ${(props: HeaderImageProps) => (props.left ? 'rotate(45deg)' : 'rotate(-45deg)')};
+`
+
+const HeaderTextContainer = styled.div`
+  display: flex;
+  align-items: center;
+`
+
+const HeaderText = styled.div`
+  color: var(--primary);
+  font-weight: 600;
+  font-size: 8rem;
+  margin: 0 3rem;
+`
+
+const HeaderSubText = styled.div`
+  color: var(--main);
+  font-weight: 600;
+  font-size: 2rem;
+  max-width: 70rem;
 `
 
 const HomePage = () => {
   return (
     <StyledHomePage>
       <Header />
+      <HeaderContainer>
+        <HeaderTextContainer>
+          <HeaderImage left src={watermelon} />
+          <HeaderText>Watermelon Farm</HeaderText>
+          <HeaderImage right src={watermelon} />
+        </HeaderTextContainer>
+        <HeaderSubText>Your happy place to farm all the watermelons of your dreams</HeaderSubText>
+      </HeaderContainer>
       <div />
     </StyledHomePage>
   )
