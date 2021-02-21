898fcf95bbb7512714ea4cfc2db6c752f3dece1f
dreamlancer <dreamlancer69@gmail.com>
2021-02-21 21:00:06 +1300

    :sparkles: create home page component

---
+++ client/src/pages/home/HomePage.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledHomePage = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+`
+
+const HomePage = () => {
+  return <StyledHomePage></StyledHomePage>
+}
+
+export default HomePage
