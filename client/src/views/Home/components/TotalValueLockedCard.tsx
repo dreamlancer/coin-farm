2d802fb6eaf28348b69b196ddf1404979e13b52e
dreamlancer <dreamlancer69@gmail.com>
2021-02-22 15:49:01 +1300

    :lipstick: update locked wording

--- client/src/views/Home/components/TotalValueLockedCard.tsx
+++ client/src/views/Home/components/TotalValueLockedCard.tsx
@@ -1,42 +1,34 @@
-import React from "react";
-import styled from "styled-components";
-import {
-  Card,
-  CardBody,
-  Heading,
-  Skeleton,
-  Text,
-} from "@pancakeswap-libs/uikit";
-import useI18n from "hooks/useI18n";
-import { useGetStats } from "hooks/api";
+import React from 'react'
+import styled from 'styled-components'
+import { Card, CardBody, Heading, Skeleton, Text } from '@pancakeswap-libs/uikit'
+import useI18n from 'hooks/useI18n'
+import { useGetStats } from 'hooks/api'
 
 const StyledTotalValueLockedCard = styled(Card)`
   align-items: center;
   display: flex;
   flex: 1;
-`;
+`
 
 const TotalValueLockedCard = () => {
-  const TranslateString = useI18n();
-  const data = useGetStats();
+  const TranslateString = useI18n()
+  const data = useGetStats()
   const tvl = data
-    ? data.total_value_locked_all.toLocaleString("en-US", {
+    ? data.total_value_locked_all.toLocaleString('en-US', {
         maximumFractionDigits: 0,
       })
-    : null;
+    : null
 
   return (
     <StyledTotalValueLockedCard>
       <CardBody>
         <Heading size="lg" mb="24px">
-          {TranslateString(762, "Total Value Locked (TVL)")}
+          Watermelons Farming
         </Heading>
         {data ? (
           <>
             <Heading size="xl">{`$${tvl}`}</Heading>
-            <Text color="textSubtle">
-              {TranslateString(764, "Across all LPs and Syrup Pools")}
-            </Text>
+            <Text color="textSubtle">TVL across all seeds and farms</Text>
           </>
         ) : (
           <>
@@ -45,7 +37,7 @@ const TotalValueLockedCard = () => {
         )}
       </CardBody>
     </StyledTotalValueLockedCard>
-  );
-};
+  )
+}
 
-export default TotalValueLockedCard;
+export default TotalValueLockedCard
