18e8e9996d37782ba5d5be04db51a749c0aefc84
dreamlancer <dreamlancer69@gmail.com>
2021-02-27 12:59:19 +1300

    :fire: remove farms footer image

--- client/src/pages/farms/FarmsPage.tsx
+++ client/src/pages/farms/FarmsPage.tsx
@@ -9,7 +9,7 @@ const StyledFarmsPage = styled.div`
   top: 0;
   left: 0;
   width: 100%;
-  height: 100%;
+  overflow-y: auto;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
